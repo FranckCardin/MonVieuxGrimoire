195dca2dea916f5334544e20e3c57468a0f1c9d2
Franck Cardin <franck.cardin@free.fr>
2023-07-27 04:37:20 +0200

    Modification Routes book.js

--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -7,16 +7,20 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 //IMPORT MIDDELEWARE MULTER
 const multer = require('../middleware/multer-config');
+//IMPORT MIDDELEWARE SHARP 
+const sharp = require('../middleware/sharp');
 
 //IMPORT CONTROLLER BOOK
 const bookCtrl = require('../controllers/book');
 
 //INITIATION DES ROUTES
-router.post('/', auth, multer, bookCtrl.addBook);
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', auth, multer, bookCtrl.updateBook);
+router.get('/bestrating', bookCtrl.getBestRatingsBooks);
+router.post('/', auth, multer, sharp, bookCtrl.addBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating', auth, bookCtrl.ratingBook);
 
 //EXPORT MODULE ROUTEUR 
 module.exports = router;
